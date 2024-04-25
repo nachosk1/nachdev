a2952ff5690920c54aba8486589cf496ed72260c
nachosk1 <104322849+nachosk1@users.noreply.github.com>
2024-04-24 20:11:04 -0400

    Swiper

--- constants/index.js
+++ constants/index.js
@@ -23,23 +23,36 @@ export const optionNav = [
 
 export const listProjects = [
   {
+    id: 1,
     title: "Cesfam Río Bueno",
     description:
       "Página web para el Centro de Salud Familiar de Río Bueno, la cual tiene como objetivo informar a la comunidad sobre los servicios que ofrece el centro y los horarios de atención. Además, dispone de una sección de noticias y un formulario para hacer feedback sobre la atención recibida.",
-    link: "https://cesfamriobueno.cl",
+    link: "https://cesfam.nachdev.com",
     github: "",
     image: "/src/cesfam.webp",
     skills: ["NextJS", "Tailwind", "MySQL", "Strapi", "Axios"],
     type: "FullStack"
   },
   {
+    id: 2,
+    title: "Car-hub - Automóviles",
+    description:
+      "Página web para entusiastas de los automóviles. Los usuarios pueden filtrar la información por marca, nombre, año y tipo de combustible, lo que facilita la búsqueda de vehículos específicos. La página consume una API de RapidAPI para obtener los datos de los vehículos, garantizando que la información esté actualizada.",
+    link: "http://carhub.nachdev.com",
+    github: "https://github.com/nachosk1/company-cars-client",
+    image: "/src/carhub.webp",
+    skills: ["NextJS", "Tailwind", "TypeScript"],
+    type: "Frontend"
+  },
+  {
+    id: 3,
     title: "Spacex Launches - Astro",
     description:
       "Página web especializada en presentar los lanzamientos de SpaceX de manera detallada y actualizada. La plataforma sirve como un centro de información para entusiastas del espacio y profesionales del sector, ofreciendo acceso a datos sobre las misiones más recientes y antiguas. ",
     link: "http://spacex-astro.nachdev.com",
     github: "https://github.com/nachosk1/spacex-launches",
     image: "/src/spacex-astro.webp",
-    skills: ["NextJS", "Tailwind", "TypeScript"],
+    skills: ["Astro", "Tailwind", "TypeScript"],
     type: "Frontend"
   },
 ];
---
+++ src/components/ListProject.jsx
@@ -0,0 +1,64 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
+
+export default function ListProyect({ listProjects }) {
+  return (
+    <div>
+      <Swiper
+        spaceBetween={30}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+        }}
+      >
+        {listProjects.map(
+          ({ id, title, description, skills, github, image, skilss, type }) => (
+            <SwiperSlide className="h-[40vh] md:h-[65vh]" key={id}>
+              <div className="shadow-lg rounded">
+                <div className="mt-4 overflow-hidden relative">
+                  <img
+                    src={image}
+                    alt={`imagen ${title}`}
+                    width={300}
+                    height={300}
+                    className="rounded-t hover:scale-105 transition duration-300 ease-in-out transform w-full h-[300px] object-cover"
+                  />
+                  <div className="absolute top-4 left-4">
+                    <span className="bg-black/25 backdrop-blur-sm rounded-full px-4 py-1 text-white">
+                      {type}
+                    </span>
+                  </div>
+                </div>
+                <div className="p-4 border">
+                  <h3 className="font-bold">{title}</h3>
+                  <p className="mt-2 text-gray-700 line-clamp-5">{description}</p>
+                  <div className="mt-4 flex gap-2 flex-wrap">
+                    {skills.map((skill) => (
+                      <span
+                        className="bg-primary text-white rounded-full px-2 py-1"
+                        key={skill}
+                      >
+                        {skill}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          )
+        )}
+      </Swiper>
+    </div>
+  );
+}
